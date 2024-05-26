baba7e60f90ff522e225ab2b15d152e7b498137e
denq113 <86840750+denq113@users.noreply.github.com>
2024-05-26 17:03:02 +0700

    hello

---
+++ WindowsFormsApp55/Figura.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp55
+{
+    public class Figura
+    {
+        int x;
+        int y;
+        int h;
+        PictureBox pb;
+
+        public Figura(int nx, int ny, int nh, PictureBox npb )
+        {
+            x = nx;
+            y = ny;
+            h = nh;
+            pb = npb;
+            Draw();
+        }
+
+        public void Draw()
+        {
+          
+            Graphics gr = Graphics.FromImage(pb.Image);
+            Pen obwodka = new Pen(Color.Red);
+            SolidBrush zalivka = new SolidBrush(Color.SkyBlue);
+            SolidBrush zalivkaG = new SolidBrush(Color.Green);
+            gr.FillRectangle(zalivka, x, y, h, h);
+            gr.DrawRectangle(obwodka, x, y, h, h);
+            gr.DrawLine(obwodka, x, y,x + h, y + h);
+            gr.DrawLine(obwodka, x + h, y, x, y + h);
+            gr.FillEllipse(zalivkaG, x + (h / 3), y + h / 20, h / 3, h / 3);
+            gr.FillEllipse(zalivkaG, x + (h / 3), y + h * 3 / 5,h / 3, h / 3);
+            pb.Refresh();
+        }
+        internal void SetPosition(int newX, int newY)
+        {
+            x = newX;
+            y = newY;
+            Draw();
+        }
+        internal void SetHeight(int newHeight)
+        {
+            h = newHeight;
+            Draw();
+        }
+    }
+}
---
+++ WindowsFormsApp55/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp55
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                int x = Convert.ToInt32(f.textBox1.Text);
+                int y = Convert.ToInt32(f.textBox2.Text);
+                int h = Convert.ToInt32(f.textBox3.Text);
+                Figura fg = new Figura(x, y, h, pictureBox1);
+                listBox1.Items.Add(fg);
+                DrawFigures();
+
+            }
+        }
+
+        private void DrawFigures()
+        {
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Refresh(); 
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                Figura fg1 = listBox1.Items[i] as Figura;
+                fg1.Draw();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Объект для удаления не выбран");
+            }
+            else
+            {
+                Figura selectedFigura = (Figura)listBox1.SelectedItem;
+                listBox1.Items.Remove(selectedFigura);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bp;
+            Graphics gr = Graphics.FromImage(bp);
+            SolidBrush solidBrush = new SolidBrush(Color.Purple);
+            gr.FillRectangle(solidBrush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                int index = listBox1.SelectedIndex;
+                if (index != -1)
+                {
+                    int x = Convert.ToInt32(f.textBox1.Text);
+                    int y = Convert.ToInt32(f.textBox2.Text);
+                    int h = Convert.ToInt32(f.textBox3.Text);
+                    Figura selectedFigure = (Figura)listBox1.Items[index];
+                    selectedFigure.SetPosition(x, y);
+                    selectedFigure.SetHeight(h);
+                    DrawFigures();
+                }
+                else
+                {
+                    MessageBox.Show("Выберите фигуру для редактирования.");
+                }
+
+
+            }
+        }
+    }
+}
